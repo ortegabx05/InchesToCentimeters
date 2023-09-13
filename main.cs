c3439306d5a830cda9c32fa27c1c0a9c037f7669
ortegabx05 <30540228+ortegabx05@users.noreply.github.com>
2023-09-13 20:07:02 +0000

    Changed variable name to InchesToCentimeters

--- main.cs
+++ main.cs
@@ -2,9 +2,9 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    const double CentimetersToInches = 2.54;
+    const double InchesToCentimeters = 2.54;
     double inches = -15;
-    double cent = inches * CentimetersToInches;
+    double cent = inches * InchesToCentimeters;
     
     Console.WriteLine ("{0} inches is {1} centimeters", inches, cent);
   }
