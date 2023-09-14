2a2a0c1580d2cc58bbb8b25486ec7e18eb18b088
ortegabx05 <30540228+ortegabx05@users.noreply.github.com>
2023-09-14 19:18:26 +0000

    Added all unit test with spaces in between

--- main.cs
+++ main.cs
@@ -3,9 +3,20 @@ using System;
 class Program {
   public static void Main (string[] args) {
     const double InchesToCentimeters = 2.54;
-    double inches = -15;
-    double cent = inches * InchesToCentimeters;
+    double inches1 = 5;
+    double cent1 = inches1 * InchesToCentimeters;
+    Console.WriteLine ("{0} inches is {1} centimeters", inches1, cent1);
+    Console.WriteLine();
+
+    double inches2 = -15;
+    double cent2 = inches2 * InchesToCentimeters;
+    Console.WriteLine ("{0} inches is {1} centimeters", inches2, cent2);
+    Console.WriteLine();
+
+    double inches3 = 15994;
+    double cent3 = inches3 * InchesToCentimeters;
     
-    Console.WriteLine ("{0} inches is {1} centimeters", inches, cent);
+    Console.WriteLine ("{0} inches is {1} centimeters", inches3, cent3);
+    Console.WriteLine();
   }
 }
